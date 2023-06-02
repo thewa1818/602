0c2b3c93def7838d3d66397b8743d264120011e7
thewa1818 <zawa.i.0218@gmail.com>
2023-06-02 14:51:19 +0900

    練習

---
+++ js/app.js
@@ -0,0 +1,18 @@
+console.log(1111);
+
+console.log("js")
+
+let num = 1;
+console.log(num , "ここにあるよ");
+
+console.log(2 + 3, "計算中");
+
+console.log("2 + 3" ,"どうなる？")
+
+
+
+$(".test").on("click", function () {
+
+  $(this).html(num);
+
+});
